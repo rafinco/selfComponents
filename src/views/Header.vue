014439f73d122718955becb2e62ce38c91515de8
maolianhuo <maolianhuo@seassoon.com>
2017-12-14 14:59:52 +0800

    add left and head

---
+++ src/views/Header.vue
@@ -0,0 +1,63 @@
+<template>
+	<div id="header">
+		<div class="logo">
+			Self Components
+			<!-- <span class="username">{{name}}</span> -->
+		</div>
+		<div class="user">
+			<img class="avatar" src="../assets/images/avatar.jpg" alt="avatar">
+			<span class="username">{{name}}</span>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default {
+	
+	data() {
+		return {
+			name: 'self components'
+		}
+	},
+	created() {
+		
+	},
+	methods: {
+		
+	}
+}
+</script>
+
+<style scoped>
+#header{
+	position: relative;
+	height: 80px;
+	line-height: 80px;
+	display: flex;
+	padding: 0 20px;
+	justify-content: space-between;
+	z-index: 12;
+}
+.logo{
+	font-size: 26px;
+	font-weight: 600;
+}
+.user{
+	font-size: 16px;
+	overflow: hidden;
+}
+.user .avatar{
+	width: 50px;
+	height: 50px;
+	border-radius: 50%;
+	vertical-align: middle;
+	margin-right: 10px;
+	display: inline-block;
+}
+.user .username{
+	display: inline-block;
+	color: #fff;
+	vertical-align: middle;
+}
+</style>
---
+++ src/views/Sidebar.vue
@@ -0,0 +1,72 @@
+<template>
+	<div class="sidebar">
+		<el-menu default-active="1-4-1" class="el-menu-vertical-demo" background-color="rgb(7,17,27)" text-color="#fff" active-text-color="#ffd04b">
+		  	<el-submenu index="1">
+			    <template slot="title">
+			      	<i class="el-icon-location"></i>
+			      	<span slot="title">导航一</span>
+			    </template>
+		      	<el-menu-item index="1-1">选项1</el-menu-item>
+		      	<el-menu-item index="1-2">选项2</el-menu-item>
+		      	<el-menu-item index="1-3">选项3</el-menu-item>
+			    <el-menu-item index="1-4">选项56</el-menu-item>
+		  	</el-submenu>
+
+		  	<el-submenu index="2">
+		  		<template slot="title">
+		  			<i class="el-icon-menu"></i>
+			    	<span slot="title">导航二</span>
+		  		</template>
+			    <el-menu-item index="2-1">选项1</el-menu-item>
+		      	<el-menu-item index="2-2">选项2</el-menu-item>
+		      	<el-menu-item index="2-3">选项3</el-menu-item>
+			    <el-menu-item index="2-4">选项56</el-menu-item>
+		  	</el-submenu>
+		  	<el-menu-item index="3">
+			    <i class="el-icon-setting"></i>
+			    <span slot="title">导航三</span>
+		  	</el-menu-item>
+		</el-menu>
+	</div>
+</template>
+
+<script>
+
+export default {
+	
+	data() {
+		return {
+			
+		}
+	},
+
+	created() {
+		
+	},
+	
+	methods: {
+		
+	}
+}
+</script>
+
+<style scoped>
+.sidebar{
+	position: fixed;
+	width: 200px;
+	height: 100%;
+	z-index: 10;
+	border-right: 1px solid #fff;
+}
+</style>
+<style>
+	.el-menu{
+		border-right: 0;
+	}
+	.el-menu-item:focus, .el-menu-item:hover{
+		background-color: #000 !important;
+	}
+	.el-submenu__title:hover{
+		background-color: #000 !important;
+	}
+</style>
